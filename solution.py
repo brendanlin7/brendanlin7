d2f67c313d42fdfde5af2e23b1524523b1e9f9e7
brendanlin7 <88155646+brendanlin7@users.noreply.github.com>
2022-04-16 15:38:08 -0700

    Add files via upload

--- solution.py
+++ solution.py
@@ -130,7 +130,7 @@ def ping(host, timeout=1):
     packet_max = max(list) * 1000
     packet_avg = statistics.mean(list) * 1000
     stdev_var = (list) * 1000
-    vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
+    vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(statistics.stdev(stdev_var), 8))]
 
     return vars
 
